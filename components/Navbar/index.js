1631dd1b5e1aa9fb6ad08a972154c42a1d059241
ambx <hibaf.me18@sahyadri.edu.in>
2021-08-15 21:16:11 +0530

    initial commit

---
+++ components/Navbar/index.js
@@ -0,0 +1,20 @@
+import styles from "./styles.module.scss";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <li className={styles.navbarItem}>
+        <a href="">about</a>
+      </li>
+      <li className={styles.navbarItem}>
+        <a href="">work</a>
+      </li>
+      <li className={styles.navbarItem}>
+        <a href="https://almost.hibafathima.com/">blog</a>
+      </li>
+      <li className={styles.navbarItem}>
+        <a href="">contact</a>
+      </li>
+    </nav>
+  );
+}
---
+++ components/Title/index.js
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export default function Title(){
+    return(
+        <h1 className={styles.title}>
+            Hiba Fathima
+        </h1>
+    )
+}
